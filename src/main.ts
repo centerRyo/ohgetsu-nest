2b4d82eb2627305665150a49a1d747f0c63f0dab
centerRyo <abc.atrn@gmail.com>
2024-04-05 14:48:57 +0900

    swaggerの導入

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config'
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get("PORT") || 3000;
-  
+  const port = configService.get('PORT') || 8080;
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('ohgetsu API')
+    .setDescription('ohgetsu API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
