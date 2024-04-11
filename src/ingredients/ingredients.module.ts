984542545f4a7acd2da0c118f8cce2eb73a629ba
centerRyo <abc.atrn@gmail.com>
2024-04-11 12:47:05 +0900

    prismaとの連結

--- src/ingredients/ingredients.module.ts
+++ src/ingredients/ingredients.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { IngredientsController } from './ingredients.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [],
-  providers: [IngredientsService],
+  providers: [IngredientsService, PrismaService],
   controllers: [IngredientsController],
   exports: [IngredientsService],
 })
--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class IngredientsService {
-  // constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    // const ingredients = await this.prisma.ingredients.findMany();
+    const ingredients = await this.prisma.ingredients.findMany();
 
-    // return ingredients;
+    return ingredients;
     return 'Hello World!!!!!!!!!!!!!!!!!';
   }
 }
