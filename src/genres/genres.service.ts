95997239964a34169bd862c5e4a9be048c0c6b68
centerRyo <abc.atrn@gmail.com>
2024-04-15 12:30:10 +0900

    genresコントローラーの作成

---
+++ src/genres/genres.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class GenresService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    const genres = await this.prisma.genres.findMany();
+
+    return genres;
+  }
+}
